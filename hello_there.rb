d0688a6b56a0060604585372a27d1647a2ac591c
Hari1610 <78408845+Hari555Y@users.noreply.github.com>
2022-08-17 09:59:46 +0000

    print puts and p statements

---
+++ hello_there.rb
@@ -0,0 +1,5 @@
+# puts is same as p.
+# diff bw print and puts
+puts "hello there 1" # cursor moves on next line
+p "hello there 2"    # cursor on next line prints with commas
+print "hello there 3" # cursor in same line
--- main.rb
+++ main.rb
@@ -1,1 +1,2 @@
-puts "Hello World"
+load "hello_there.rb"
+
